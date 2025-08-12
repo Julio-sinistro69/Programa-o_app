77d4d3946707a4268b6e00ccd3a445de69f3f6e3
unknown <you@example.com>
2025-08-12 10:45:09 -0300

    mensage

--- funcoes_2.js
+++ funcoes_2.js
@@ -1,16 +1,35 @@
 const prompt = require('prompt-sync')();
-let num1;
-let num2;
-let opcao;
-prompt = console.log("digite um número:");
-prompt = console.log("digite um número:");
-console.log("Digite 1 para somar, 2 para subtrair, 3 para multiplicar e 4 para dividir");
 
-function escolher(opcao) {
-    if (opcao == 1) {
-        
+
+let num1 = Number(prompt("digiite um número:"));
+let num2 = Number (prompt("digite um número:"));
+let opcao = Number(prompt("Digite 1 para somar, 2 para subtrair, 3 para multiplicar e 4 para dividir: "));
+console.log(escolher(opcao, num1, num2));
+    
+function escolher(opcao, num1, num2) {
+    switch (opcao) {
+        case 1:
+            return somar(num1, num2);
+        case 2:
+            return subtrair(num1, num2);
+        case 3:
+            return multiplicar(num1, num2);    
+        case 4:
+            return dividir(num1, num2)    
+            default:
+                console.log("Opção inválidda");
+            break;
     }
 }
 function somar(num1, num2) {
     return num1 + num2;
+}
+function subtrair(num1, num2) {
+    return num1 - num2;
+}
+function multiplicar(num1, num2) {
+    return num1 * num2;
+}
+function dividir(num1, num2) {
+    return num1/num2;
 }
